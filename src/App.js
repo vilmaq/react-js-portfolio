239ac164197392d868f88925867a217a2cdb584e
Vilma Qerama <vilma.qerama@hotmail.com>
2021-07-27 14:36:51 +0100

    Add the portfolio structure in the App.js

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+export default function App() {
+  return (
+    <main>
+      <Navbar />
+      <About />
+      <Experience />
+      <Education />
+      <Skills />
+      <Interests />
+      <Awards />
+      <Portfolio />
+      <CV />
+    </main>
+  );
+}
