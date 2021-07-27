b04537b92223b03bf21b6087dae35f73620ba6bd
Vilma Qerama <vilma.qerama@hotmail.com>
2021-07-27 15:04:07 +0100

    use Tailwind CSS for general styling (backgrond and font)

--- src/App.js
+++ src/App.js
@@ -12,7 +12,7 @@ import Contact from "./components/Contact";
 
 export default function App() {
   return (
-    <main>
+    <main className="text-gray-400 bg-gray-900 body-font">
       <Navbar />
       <About />
       <Experience />
