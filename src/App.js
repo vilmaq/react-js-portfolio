4ce701aaeb997a9d50fe111ed187825393780eaa
Vilma Qerama <vilma.qerama@hotmail.com>
2021-07-27 14:49:06 +0100

    create components and import, export them in the App.js

--- src/App.js
+++ src/App.js
@@ -1,4 +1,14 @@
 import React from "react";
+import Navbar from "./components/Navbar";
+import About from "./components/About";
+import Experience from "./components/Experience";
+import Education from "./components/Education";
+import Skills from "./components/Skills";
+import Interests from "./components/Interests";
+import Awards from "./components/Awards";
+import Portfolio from "./components/Portfolio";
+import Cv from "./components/Cv";
+import Contact from "./components/Contact";
 
 export default function App() {
   return (
@@ -11,7 +21,8 @@ export default function App() {
       <Interests />
       <Awards />
       <Portfolio />
-      <CV />
+      <Cv />
+      <Contact />
     </main>
   );
 }
