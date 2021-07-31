a3b64d3b2fa67c5ee35505e89d99be40097aef46
Vilma Qerama <vilma.qerama@hotmail.com>
2021-08-01 00:41:04 +0100

    fix footer heght

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -2,36 +2,43 @@ import React from "react";
 
 export default function Footer() {
   return (
-    <footer className="h-10 bg-blue-500">
+    <footer className="h-20 bg-yellow-400 width: 100%">
       <div
-        className="pt-4 md:flex md:items-center md:justify-center "
-        style={{ borderTop: "1px solid white" }}
+        className="pt-4 md:flex md:items-up md:justify-center "
+        style={{ borderTop: "0px solid white" }}
       >
         <ul className="">
           <li
             className="md:mx-2 md:inline leading-7 text-sm"
             id="footer-navi-2"
           >
-            <a className="text-white underline text-small" href="/disclaimer">
-              <i className="fab fa-github fa-2x"></i>
+            <a
+              className="text-white underline text-small"
+              href="https://github.com/vilmaq"
+            >
+              <i className="fab fa-github fa-3x"></i>
             </a>
           </li>
           <li
             className="md:mx-2 md:inline leading-7 text-sm"
             id="footer-navi-2"
           >
-            <i className="fab fa-github">
-              <a className="text-white underline text-small" href="/cookie">
-                Linkedin
-              </a>
-            </i>
+            <a
+              className="text-white underline text-small"
+              href="https://www.linkedin.com/in/vilmaqerama/"
+            >
+              <i class="fab fa-linkedin-in fa-3x"></i>
+            </a>
           </li>
           <li
             className="md:mx-2 md:inline leading-7 text-sm"
             id="footer-navi-2"
           >
-            <a className="text-white underline text-small" href="/privacy">
-              Privacy
+            <a
+              className="text-white underline text-small"
+              href="https://twitter.com/V174A"
+            >
+              <i class="fab fa-twitter fa-3x"></i>
             </a>
           </li>
         </ul>
