a7eee1a36e3b94862c5ed184510b580a86ecd2fc
Vilma Qerama <vilma.qerama@hotmail.com>
2021-08-01 15:15:21 +0100

    add logo in the header

--- src/components/Header.js
+++ src/components/Header.js
@@ -6,8 +6,8 @@ export default function Header({ children }) {
     <header className="bg-gray-800 md:sticky top-0 z-10">
       <div className="container mx-auto flex flex-wrap p-5 flex-col md:flex-row items-center">
         <a className="title-font font-medium text-white mb-4 md:mb-0">
-          <a href="#about" className="ml-3 text-xl">
-            V
+          <a href="#about">
+            <img src="./logV.png" />
           </a>
         </a>
         {children}
